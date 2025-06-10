4daea08cc1963287aef7c515c356f85805100242
Krunalchavhan26 <krunalchavhan5851@gmail.com>
2025-06-10 18:46:54 +0530

    Add refresh access token controller

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -11,5 +12,7 @@ const router = Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
