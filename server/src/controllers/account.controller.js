e373b03889ee201cb16cc9314f4d2bedd3789bae
Krunalchavhan26 <krunalchavhan5851@gmail.com>
2025-06-11 14:30:37 +0530

    Add edit-account controller

--- server/src/controllers/account.controller.js
+++ server/src/controllers/account.controller.js
@@ -4,8 +4,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { Account } from "../models/account.model.js";
 
 const addAccount = asyncHandler(async (req, res) => {
-  const { name, type, startingBalance, goal, commission } =
-    req.body;
+  const { name, type, startingBalance, goal, commission } = req.body;
 
   if (
     [name, type, startingBalance, goal, commission].some(
@@ -18,10 +17,10 @@ const addAccount = asyncHandler(async (req, res) => {
 
   const alreadyExists = await Account.findOne({
     name: name,
-    ownedBy: req.user._id
-  })
+    ownedBy: req.user._id,
+  });
 
-  if(alreadyExists) {
+  if (alreadyExists) {
     throw new ApiError(409, "Account with this name already exists");
   }
 
@@ -32,10 +31,50 @@ const addAccount = asyncHandler(async (req, res) => {
     goal,
     commission,
     currentBalance: startingBalance, // Initially same as starting
-    ownedBy: req.user._id
-  })
+    ownedBy: req.user._id,
+  });
 
-  return res.status(201).json(new ApiResponse(201, account, "Account created successfully"))
+  return res
+    .status(201)
+    .json(new ApiResponse(201, account, "Account created successfully"));
 });
 
-export {addAccount};
+const editAccount = asyncHandler(async (req, res) => {
+  const { accountId } = req.params;
+  const { name, type, startingBalance, goal, accountStatus, commission } =
+    req.body;
+
+  const account = await Account.findOne({
+    _id: accountId,
+    ownedBy: req.user._id,
+  });
+
+  if (!account) throw new ApiError(404, "Account not found or accessable");
+
+  // only update the provided fields
+  if (name !== undefined) account.name = name;
+  if (type !== undefined) account.type = type;
+  if (goal !== undefined) account.goal = goal;
+  if (accountStatus !== undefined) account.accountStatus = accountStatus;
+  if (commission !== undefined) account.commission = commission;
+
+  if (startingBalance !== undefined) {
+    if (account.totalTrades === 0) {
+      account.startingBalance = startingBalance;
+      account.currentBalance = startingBalance;
+    } else {
+      throw new ApiError(
+        400,
+        "Cannot update starting balance after trades have been placed"
+      );
+    }
+  }
+
+  await account.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, account, "Account updated successfully"));
+});
+
+export { addAccount, editAccount };
--- server/src/routes/account.route.js
+++ server/src/routes/account.route.js
@@ -1,11 +1,12 @@
 import {Router} from "express"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addAccount } from "../controllers/account.controller.js";
+import { addAccount, editAccount } from "../controllers/account.controller.js";
 
 
 const router = Router();
 
 router.route("/add-account").post(verifyJWT, addAccount)
+router.route("/edit-account/:accountId").patch(verifyJWT, editAccount);
 
 export default router;
 
